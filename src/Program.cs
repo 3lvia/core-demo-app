b6a32fe9736d150f02106dd4eeb6c0c6b83fba4f
Øystein Thuen <44778126+OysteinThuen@users.noreply.github.com>
2021-04-08 13:24:52 +0200

    Add simple app

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace core_demo_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
