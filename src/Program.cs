d20c2150cd6b9a8df7d6b21a4b81defd8a6861bb
Øystein Thuen <44778126+OysteinThuen@users.noreply.github.com>
2023-09-16 10:40:34 +0200

    Call kubernetes-api a lot of times

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using k8s;
 
 namespace core_demo_app
 {
@@ -6,10 +8,25 @@ namespace core_demo_app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting core-demo-app");
+            var config = KubernetesClientConfiguration.InClusterConfig();
+            var client = new Kubernetes(config);
             while (true)
             {
-                System.Threading.Thread.Sleep(10);
+                var name = "";
+                for (int i = 0; i < 1000; i++)
+                {
+
+                    var result = client.CoreV1.ListNamespacedConfigMapWithHttpMessagesAsync("vault").Result;
+                    result.Response.EnsureSuccessStatusCode();
+                    name = result.Body.Items
+                        .Select(cm => cm.Metadata.Name)
+                        .FirstOrDefault();
+                    System.Threading.Thread.Sleep(10);
+                }
+
+                Console.WriteLine(DateTime.Now.ToString("u") + ": " + name);
+                System.Threading.Thread.Sleep(1000);
             }
         }
     }
