d16d8c1474fcb5bd131ee455e7d7d92c0b2580cd
Øystein Thuen <44778126+OysteinThuen@users.noreply.github.com>
2023-10-03 11:06:56 +0200

    Remove kubernetes calls. Add Elvia.Configuration

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Linq;
-using k8s;
 
 namespace core_demo_app
 {
@@ -9,23 +7,9 @@ namespace core_demo_app
         static void Main(string[] args)
         {
             Console.WriteLine("Starting core-demo-app");
-            var config = KubernetesClientConfiguration.InClusterConfig();
-            var client = new Kubernetes(config);
+            Console.WriteLine($"Instrumentation key: {Elvia.Configuration.HashiVault.HashiVault.EnsureHasValue("core/kv/appinsights/core/instrumentation-key")}");
             while (true)
             {
-                var name = "";
-                for (int i = 0; i < 1000; i++)
-                {
-
-                    var result = client.CoreV1.ListNamespacedConfigMapWithHttpMessagesAsync("vault").Result;
-                    result.Response.EnsureSuccessStatusCode();
-                    name = result.Body.Items
-                        .Select(cm => cm.Metadata.Name)
-                        .FirstOrDefault();
-                    System.Threading.Thread.Sleep(10);
-                }
-
-                Console.WriteLine(DateTime.Now.ToString("u") + ": " + name);
                 System.Threading.Thread.Sleep(1000);
             }
         }
